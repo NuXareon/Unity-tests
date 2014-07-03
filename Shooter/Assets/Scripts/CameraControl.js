775e306ad7f7ef3c3452c129e61207187bdb99cf
NuXareon <millas@gmail.com>
2014-07-03 13:59:51 +0200

    Shooter - 1

---
+++ Shooter/Assets/Scripts/CameraControl.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var player : Transform;
+
+function Start () {
+
+}
+
+function LateUpdate () {
+	transform.position = player.position+Vector3(0.0f,0.5f,0.0f);
+	transform.rotation.eulerAngles.y = player.rotation.eulerAngles.y;
+	transform.rotation.eulerAngles.x = player.rotation.eulerAngles.x;
+}
---
+++ Shooter/Assets/Scripts/PlayerControl.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+private var controller : CharacterController;
+private var direction : Vector3 = Vector3.zero;
+
+var moveSpeed : float;
+var turnSpeed : float;
+var jumpSpeed : float;
+var gravity : float;
+
+function Start () {
+	controller = GetComponent("CharacterController");
+}
+
+function Update () {
+	transform.rotation.eulerAngles.y += Time.deltaTime*Input.GetAxis("Mouse X")*turnSpeed;
+	transform.rotation.eulerAngles.x += Time.deltaTime*Input.GetAxis("Mouse Y")*turnSpeed;
+	
+	if (controller.isGrounded)
+	{
+		direction = Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		direction = transform.TransformDirection(direction);
+		direction *= moveSpeed;
+		//direction.y = 0;
+		
+		if (Input.GetButton("Jump"))
+		{
+			direction.y = jumpSpeed;
+		}
+	} else
+	{
+		var oldY = direction.y;
+		direction = Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		direction = transform.TransformDirection(direction);
+		direction *= moveSpeed;
+		direction.y = oldY;
+	}
+
+	if (direction.y > 0 || !controller.isGrounded) direction.y -= gravity;
+	controller.Move(direction*Time.deltaTime);
+}
